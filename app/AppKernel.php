cf0fe6584a0bc5a7c969dae2fb02fe4d714969a2
Michaël Perrin <michael@perrin.se>
2014-09-24 17:05:43 +0200

    Create front bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Liip\ImagineBundle\LiipImagineBundle(),
+            new RadioNeo\FrontBundle\RadioNeoFrontBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/RadioNeo/FrontBundle/RadioNeoFrontBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace RadioNeo\FrontBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RadioNeoFrontBundle extends Bundle
+{
+}
